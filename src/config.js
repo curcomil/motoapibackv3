dda11054eb41d06c7db55bf56a11133237766c2d
curcomil <sadielsalgado98@gmail.com>
2024-08-19 16:07:41 -0600

    front cors

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = "https://motoapiv3.vercel.app/";
