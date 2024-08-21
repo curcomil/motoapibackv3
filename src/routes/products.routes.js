239122baf53c467363df2258256b183247f3a41f
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-20 21:19:38 -0600

    Update products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -10,6 +10,7 @@ import {
   updateProductById,
   deleteProductById,
   addResponse,
+  reduceProductStock,
 } from "../controllers/products.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 
@@ -72,5 +73,5 @@ router.post(
 );
 router.put("/products/:id", updateProductById);
 router.delete("/products/:id", deleteProductById);
-
+router.put("/products/:id/reduce-stock", reduceProductStock);
 export default router;
