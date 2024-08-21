ff279c9d64f0aa7b8bfc34e69939be0edb53fa7e
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-21 14:15:10 -0600

    try webhook

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -10,7 +10,6 @@ import {
   updateProductById,
   deleteProductById,
   addResponse,
-  reduceProductStock,
 } from "../controllers/products.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 
@@ -73,5 +72,4 @@ router.post(
 );
 router.put("/products/:id", updateProductById);
 router.delete("/products/:id", deleteProductById);
-router.put("/products/:id/reduce-stock", reduceProductStock);
 export default router;
